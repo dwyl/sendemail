61ee84ac46df5ab810b05ce8c20c2cd28abf0325
Nelson <contact.nelsonic@gmail.com>
2015-10-07 07:21:13 +0100

    adds env2 https://github.com/dwyl/env2 and re-factor tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,24 +1,27 @@
-var test   = require('tape');
+var config = require('path').resolve(__dirname+'/../config.env');
+var env   = require('env2')(config);
+// console.log(' >>>> ' +process.env.MANDRILL_APIKEY)
+var test  = require('tape');
 var dir   = __dirname.split('/')[__dirname.split('/').length-1];
 var file  = dir + __filename.replace(__dirname, '');
-var uncache = require('./uncache').uncache;
+var decache = require('decache');
 
-uncache('../lib/email_welcome'); // clear cached email module
+// decache('../lib/'); // clear cached email module
 var APIKEY = process.env.MANDRILL_APIKEY; // store for later
 process.env.MANDRILL_APIKEY = null; // delete key to force fail
-var email  = require('../lib/email_welcome'); // no api key
+var email  = require('../lib/'); // no api key
 
 test(file+" Force Fail in Email", function(t) {
   var person = {
     "email"    : 'bad@example.com',
     "password" : "thiswill400"
-  }
+  };
   email(person, function(eres) {
     setTimeout(function(){
       console.log(eres);
       t.equal(eres.status, 'error', "Invalid Mandrill Key");
       process.env.MANDRILL_APIKEY = APIKEY; // restore key for next tests
-      uncache('../lib/email_welcome'); // clear cached email module
+      // decache('../lib/email_welcome'); // clear cached email module
       t.end();
     },50);
   })
@@ -30,14 +33,16 @@ test(file+" Email Successfully Sent ", function(t) {
   var person = {
     "email"    : 'dwyl.test+email_welcome' +Math.random()+'@gmail.com',
     "password" : "NotRequiredToTestEmail!"
-  }
+  };
   process.env.MANDRILL_APIKEY = APIKEY; // restore key for next tests
-  uncache('../lib/email_welcome'); // clear cached email module
-  var email  = require('../lib/email_welcome'); // WITH api key
+  // decache('../lib/email_welcome'); // clear cached email module
+  var email  = require('../lib/'); // WITH api key
   email(person, function(eres){
+    console.log(' - - - - - - - - - - - - - - - - - - - ')
     console.log(eres);
+    console.log(' - - - - - - - - - - - - - - - - - - - ')
     t.equal(eres[0].status, 'sent', "Email Sent "+eres[0]._id);
-    uncache('../lib/email_welcome'); // clear cached email module
+    // decache('../lib/email_welcome'); // clear cached email module
     t.end();
   })
 });

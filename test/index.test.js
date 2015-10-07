e29f2dd8b957c55b925bc0dc3a16faad90dcc38d
Nelson <contact.nelsonic@gmail.com>
2015-10-07 07:48:20 +0100

    :sweat_smile: #finally got 100% again. now to re-factor and make it generic!

--- test/index.test.js
+++ test/index.test.js
@@ -1,15 +1,15 @@
-var config = require('path').resolve(__dirname+'/../config.env');
-var env   = require('env2')(config);
-// console.log(' >>>> ' +process.env.MANDRILL_APIKEY)
-var test  = require('tape');
-var dir   = __dirname.split('/')[__dirname.split('/').length-1];
-var file  = dir + __filename.replace(__dirname, '');
+var config  = require('path').resolve(__dirname+'/../config.env');
+var env     = require('env2')(config);
+var test    = require('tape');
+var dir     = __dirname.split('/')[__dirname.split('/').length-1];
+var file    = dir + __filename.replace(__dirname, '');
 var decache = require('decache');
 
-// decache('../lib/'); // clear cached email module
+decache('../lib/index.js'); // clear cached email module
+console.log("MANDRILL_APIKEY >>> "+process.env.MANDRILL_APIKEY)
 var APIKEY = process.env.MANDRILL_APIKEY; // store for later
 process.env.MANDRILL_APIKEY = null; // delete key to force fail
-var email  = require('../lib/'); // no api key
+var email  = require('../lib/index.js'); // no api key
 
 test(file+" Force Fail in Email", function(t) {
   var person = {
@@ -21,7 +21,7 @@ test(file+" Force Fail in Email", function(t) {
       console.log(eres);
       t.equal(eres.status, 'error', "Invalid Mandrill Key");
       process.env.MANDRILL_APIKEY = APIKEY; // restore key for next tests
-      // decache('../lib/email_welcome'); // clear cached email module
+      // decache('../lib/'); // clear cached email module
       t.end();
     },50);
   })
@@ -34,9 +34,12 @@ test(file+" Email Successfully Sent ", function(t) {
     "email"    : 'dwyl.test+email_welcome' +Math.random()+'@gmail.com',
     "password" : "NotRequiredToTestEmail!"
   };
+  console.log("MANDRILL_APIKEY >>> "+APIKEY)
   process.env.MANDRILL_APIKEY = APIKEY; // restore key for next tests
-  // decache('../lib/email_welcome'); // clear cached email module
-  var email  = require('../lib/'); // WITH api key
+  console.log("MANDRILL_APIKEY >>> "+process.env.MANDRILL_APIKEY)
+
+  decache('../lib/index.js'); // clear cached email module
+  var email  = require('../lib/index.js'); // WITH api key
   email(person, function(eres){
     console.log(' - - - - - - - - - - - - - - - - - - - ')
     console.log(eres);

31b2fea4e230b854dca66f375261844450f51dbb
newswim <ofenixculpa@gmail.com>
2017-02-07 11:57:51 -0600

    Fixing tests, add npm-debug.log to .gitignore

--- test/index.test.js
+++ test/index.test.js
@@ -19,7 +19,7 @@ test(file+" set_template_directory without args throws error!", function(t) {
   try {
     sendemail.set_template_directory();
   } catch (e) {
-    t.ok(e.indexOf('Please Set a Template Directory') > -1, 'Error Thrown: '+e)
+    t.equal(e.message, 'Please Set a Template Directory')
     t.equal(process.env.TEMPLATE_DIRECTORY, undefined, "Not Set (as expected)");
     t.end();
   }
@@ -50,21 +50,21 @@ test(file+" attempt to compile non-existent template (fail!)", function(t) {
 var Handlebars = require('handlebars');
 
 test(file+" compile a known template", function(t) {
-  var c = sendemail.compile_template('hello', 'html');
+  var c = sendemail.compile_template('hello.html');
   var result = c({name:'Jimmy'});
   t.ok(result.indexOf("<p>Hello Jimmy!</p>") > -1, 'Rendered: '+result);
   t.end()
 });
 
 test(file+" compile .html template from cache", function(t) {
-  var c = sendemail.compile_template('hello', 'html');
+  var c = sendemail.compile_template('hello.html');
   var result = c({name:'Jenny'});
   t.ok(result.indexOf("<p>Hello Jenny!</p>") > -1, 'Rendered: '+result);
   t.end()
 });
 
 test(file+" compile .txt template", function(t) {
-  var c = sendemail.compile_template('hello', 'txt');
+  var c = sendemail.compile_template('hello.txt');
   var result = c({name:'Jenny'});
   t.ok(result.indexOf("Hello Jenny!") > -1, 'Rendered: '+result);
   t.end()
@@ -74,9 +74,9 @@ test(file+" Force Fail in Email", function(t) {
   var person = {
     "name"     : "Bounce",
     "email"    : "invalid.email.address",
-    "subject":"Welcome to DWYL :)"
+    "subject"  : "Welcome to DWYL :)"
   };
-  email('hello', person, function(err, data) {
+  email('hello.html', person, function(err, data) {
     // console.log(' - - - - - - - - - - - ');
     // console.log(err, data);
     t.equal(err.statusCode, 400, "Invalid Mandrill Key");
@@ -90,7 +90,7 @@ test(file+" send email (Success)", function(t) {
     email: "success@simulator.amazonses.com",
     "subject":"Welcome to DWYL :)"
   }
-  email('hello', person, function(err, data){
+  email('hello.html', person, function(err, data){
     // console.log(err, data);
     t.ok(data.MessageId.length > 0, 'Email Sent!');
     t.end()
